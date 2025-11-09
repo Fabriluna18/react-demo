018bfe52c54fdb7ca65adcc776a74ba933aeb6a6
Fabriluna18 <fabrizioluna715@gmailcom>
2025-11-09 19:34:17 -0300

    refactor(Button.jsx): cambiar el color del boton

--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -3,7 +3,12 @@ import React from "react"
  export default function button({text, funcionalidad}) {
   return (
     <>
-      <button onClick={() => funcionalidad()}>{text}</button>
+      <button 
+          onClick={() => funcionalidad()} 
+          style={{ backgroundColor: "orange", color: "white", border: "none", padding: "8px 16px", borderRadius: "5px", cursor: "pointer", margin:"5px" }}
+          >
+          {text}
+      </button>
     </>
   )
 }
